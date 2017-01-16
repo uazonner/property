611d0253b5ff305fe17a26513be3b3175b0f39b4
Alexey Kobyleha <uazonner@gmail.com>
2017-01-17 01:55:24 +0200

    new header

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,97 @@
-$(window).on('resize',function() {
-    if ($(window).width() < 1000) {
-        $('#headerfull').hide();
-        $('#navigationfull .primary').hide();
-        $('#navigationfull .primary').addClass('responsive');
-        $('#navigationfull .primary').removeClass('primary');
+/* Selectors */
+
+var navigation = ".navigation ul";
+var toogleMenu = "#toggle-menu";
+var tracker = "nav .tracker";
+var slickNav = ".slicknav_menu";
+var menu = "#menu";
+
+function init() {
+
+    /* < 991 PX Responsive */
+    if (isMobile()) {
+        setMenuAttr();
     } else {
-        $('#headerfull').show();
+        resetMenuAttr();
     }
 
+    /* < 320 PX Responsive */
+    if (isMobile(true)) {
+        hideTracker();
+    } else {
+        showTracker();
+    }
+}
 
-});
-
-$(document).ready(function() {
-    $(window).trigger('resize');
-
-    $("#menu-toggle").on( "click", function() {
-        $('#navigationfull .responsive').show();
+function hideTracker() {
+    $( tracker ).hide();
+    $( toogleMenu ).css({
+        "text-align": "center",
+        "float": "none"
     });
+}
+
+function showTracker() {
+    $( tracker ).show();
+    $( toogleMenu ).css({
+        "text-align": "center",
+        "float": "right"
+    });
+}
+
+function isMobile(isMin) {
+    if (isMin === true) {
+        return $( window ).width() <= 320;
+    } else {
+        return $( window ).width() <= 991;
+    }
+}
+
+function setMenuAttr() {
+    $(navigation).hide();
+    $( slickNav ).show();
+}
+
+function resetMenuAttr() {
+    $( slickNav ).hide();
+    $(navigation).show();
+}
+
+$( document ).ready(function() {
+    console.log( "Page ready!" );
+
+    $( window ).resize(function() {
+        init();
+    });
+
+    init();
+
+    $( menu ).slicknav({
+        label: '',
+        prependTo:'.navigation',
+        duration: 500,
+        duplicate: true
+    });
+
+    /* < 991 PX Responsive */
+    if (isMobile()) {
+        $( slickNav ).show();
+    }
+
+    if (!isMobile()) {
+
+    }
+
 });
+
+
+
+
+
+
+
+
+
+
+
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <title>Property Project</title>
@@ -11,56 +11,75 @@
     <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap.min.css') !!}" />
     <link rel="stylesheet" type="text/css" href="{!! asset('css/bootstrap-theme.min.css') !!}" />
     <link rel="stylesheet" type="text/css" href="{!! asset('css/font-awesome.min.css') !!}" />
+    <link rel="stylesheet" type="text/css" href="{!! asset('css/slicknav.css') !!}" />
 
     <script src="{!! asset('js/jquery.min.js') !!}"></script>
     <script src="{!! asset('js/script.js') !!}"></script>
     <script src="{!! asset('js/bootstrap.min.js') !!}"></script>
+    <script src="{!! asset('js/jquery.slicknav.min.js') !!}"></script>
+
 </head>
 <body>
-    <div id="header">
-        <header id="headerfull">
-            <div class="container">
-                <div id="site-info">
-                    <div class="slogan">Недвижимость Украины</div>
-                    <div class="site-name"><a href="#">Real.kh.ua</a></div>
-                </div>
-            </div>
-        </header>
-        <nav id="navigationfull">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" id="menu-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <div class="mobile-site-info">
-                        <a class="slogan-mobile hidden-lg hidden-md hidden-sm" href="#">Недвижимость Украины</a>
-                        <a class="site-name-mobile hidden-lg hidden-md hidden-sm" href="#">Real.kh.ua</a>
-                    </div>
-                    <button type="button" id="user-toggle" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                        <span class="sr-only">Toggle navigation</span>
-                        <i class="fa fa-user-o"></i>
-                    </button>
-                </div>
-
-
-
-                <div class="tracker">
-                    <a class="main-nav-link" href="#"><i class="fa fa-newspaper-o" aria-hidden="true"></i>
-                        2 Апреля 2017, 20:46
-                    </a>
-                </div>
-                <div class="primary">
-                    <ul>
-                        <li class="active"><a href="#">Главная <i class="fa fa-angle-down"></i></a></li>
-                        <li><a href="#">Недвижимость <i class="fa fa-angle-down"></i></a></li>
-                        <li><a href="#">Статьи <i class="fa fa-angle-down"></i></a></li>
-                    </ul>
-                </div>
-            </div>
-
-
-        </nav>
+<header>
+    <!-- Site info -->
+    <div class="container">
+        <div class="site-info">
+            <div class="slogan">Недвижимость Украины</div>
+            <div class="site-name"><a href="#">Real.kh.ua</a></div>
+        </div>
     </div>
+    <!-- Navigation -->
+    <nav>
+        <div class="container">
+            <div class="tracker">
+                <a href="#"><i class="fa fa-newspaper-o" aria-hidden="true"></i>
+                    2 Апреля 2017, 20:46
+                </a>
+            </div>
+
+            <div class="navigation">
+
+               {{-- <a id='toggle-menu' class="visible-xs visible-sm" href="#"><i class="fa fa-bars" aria-hidden="true"></i></a>--}}
+
+                <ul id="menu" class="hidden-xs hidden-sm responsive-menu">
+                    <li class="first active"><a href="#">Новостройки</a></li>
+                    <li><a href="#">Застройщики</a></li>
+                    <li><a href="#">Спецпредложения</a></li>
+                    <li class="last"><a href="#">Аналитика</a></li>
+                </ul>
+            </div>
+
+
+        </div>
+        {{--<!-- Mobile Nav -->
+        <div class="navigation-mobile visible-xs">
+            <div class="site-info-mobile">
+                <a class="slogan-mobile" href="#">Недвижимость Украины</a>
+                <a class="site-name-mobile" href="#">Realty.com.ua</a>
+            </div>
+
+            <button type="button" id="menu-toggle" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-exampl2e">
+                <span class="sr-only">Toggle navigation</span>
+                <i class="fa fa-bars" aria-hidden="true"></i>
+            </button>
+
+            <button type="button" id="user-toggle" class="navbar-toggle" data-toggle="collapse"
+                    data-target="#bs-example">
+                <span class="sr-only">Toggle navigation</span>
+                <i class="fa fa-user-o" aria-hidden="true"></i>
+            </button>
+        </div>--}}
+    </nav>
+</header>
+<!-- Main Page -->
+<main>
+    <div class="container">
+
+            <div class="col-lg-9 col-md-9 content">Центр</div>
+            <div class="col-lg-3 col-md-3 sidebar">Правый блок</div>
+
+    </div>
+</main>
 </body>
 </html>
